6cc749ddf85bcf1fe5cb534318100e881633e21d
ved4a <vedaas.anand@gmail.com>
2024-03-13 17:19:45 +0530

    feat: Create function declarations for solve, empty, and valid

--- sudoku.c
+++ sudoku.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 // for my own reference, 1 is 'success' and 0 is 'failure'
+int is_valid(int[][9], int x, int y, int z);
+int solve_sudoku(int[][9]);
+int is_empty(int[][9], int *, int *);
 
 int game[9][9] =
     {{1, 7, 4, 0, 9, 0, 6, 0, 0},
@@ -12,3 +15,7 @@ int game[9][9] =
      {2, 8, 6, 9, 0, 0, 0, 0, 1},
      {0, 0, 0, 6, 2, 7, 0, 3, 8},
      {0, 5, 3, 0, 8, 0, 0, 9, 6}};
+
+int is_empty(int[][9], int *, int *)
+{
+}
