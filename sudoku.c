eccce0aaa7f4a26faa42db81aa01b5e739c5a987
ved4a <vedaas.anand@gmail.com>
2024-03-13 16:21:45 +0530

    fix: Loop

--- sudoku.c
+++ sudoku.c
@@ -29,9 +29,9 @@ int main(void)
 
 void print_sudoku(void)
 {
-    for (int i = 0; i < 0; i++)
+    for (int i = 0; i < 8; i++)
     {
-        for (int j = 0; j < 0; j++)
+        for (int j = 0; j < 8; j++)
         {
             printf("%d", sudoku[i][j]);
         }
