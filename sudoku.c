7a2e4896c98333d9bcc4234794b6d04ea6d9541c
ved4a <vedaas.anand@gmail.com>
2024-03-13 14:15:03 +0530

    feat: Create function to check if column is empty

--- sudoku.c
+++ sudoku.c
@@ -3,6 +3,11 @@
 void print_sudoku(void);
 int solve_sudoku(int, int);
 
+int check_column(int, int, int);
+int check_row(int, int, int);
+int check_square(int, int, int);
+
+
 int main() {
     int x = 0;
     int y = 0;
@@ -21,5 +26,15 @@ void print_sudoku(void) {
 }
 
 int solve_sudoku(int, int) {
-    
+    int num = 1;
+
+}
+
+int check_column(int x, int y, int num) {
+    for (int i = 0 ; i < 8; i++){
+        if (sudoku[x][i] == num) {
+            return 1;
+        }
+    }
+    return 0;
 }
